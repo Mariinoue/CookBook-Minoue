55d09dc276508a12d69b574981077c16a7d701ab
Mariana Inoue <marinoue@gmail.com>
2021-09-05 14:29:06 -0300

    mongodb-uri

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,9 @@
 const mongoose=require('mongoose')
 
+const MONGO_URL=process.env.MONGODB_URI 
+
 const connect=()=>
-    {mongoose.connect('mongodb://localhost:27017/cookbook',
+    {mongoose.connect(MONGO_URL,
 
     {
         useNewUrlParser:true,
